46fa59d79a954e937ead1aa84329860d4491ab7b
Logan Wood <lwood3499@gmail.com>
2020-11-14 10:44:56 -0600

    updated codon chart instructions for students

--- src/routes/codonchart.svelte
+++ src/routes/codonchart.svelte
@@ -3,7 +3,14 @@
 </script>
 
 <div class="codonapp_container">
-  <h1 class="side_container">Stuff over here</h1>
+  <div class="side_container">
+    <h1>Instructions:</h1>
+    <p>1) Click first base pair button on left.</p>
+    <p>2) Click second base pair button at the top.</p>
+    <p>3) Click third base pair button in the highlighted row.</p>
+    <p>4) Observe the amino acid that is highlighted by all selections.</p>
+    <p>5) Put your answer below the codon and click submit!</p>
+  </div>
   <CodonChart />
   <div class="side_container" />
 </div>
@@ -20,7 +27,7 @@
   .side_container {
     display: flex;
     justify-content: center;
-    /* align-items: center; */
+    flex-direction: column;
     width: 175px;
     height: 500px;
   }
