ba4b22802abf40d36e0120ddd62ca25a488c37c9
Logan Wood <lwood3499@gmail.com>
2020-11-13 21:00:49 -0600

    title update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <svelte:head>
-  <title>Mr. Wood's Biology Page</title>
+  <title>Mr. Wood's Biology Site</title>
 </svelte:head>
 <div class="home_container" in:fade={{ delay: 500, duration: 200 }}>
   <h1 class="title">Biology Text Content Coming Soon...</h1>
